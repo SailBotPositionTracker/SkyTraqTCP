45cb7f866a09ca0bc474eaaa2c3e84fd5a934742
Tom Frikker <tom.frikker@gmail.com>
2018-11-29 20:18:05 -0500

    Changed client

--- tcp_client.py
+++ tcp_client.py
@@ -1,31 +1,45 @@
 import socket
 import sys
+import venus6 as v
+
+dev = "/dev/ttyUSB0"
+baud = 115200
+gps = v.Venus6(dev,baud)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.1.234', 5008)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+server_address = ('192.168.1.111', 5010)
+print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
-try:
+print("Probable serial speed: " + str(gps.guessSerialSpeed()))
 
-    # Send data
-    message = 'I am a sailboat gimme rtk stuff plz thank u'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
+while True:
+    try:
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-	if (not (data == "")):
-            print >>sys.stderr, 'received "%s"' % data
+        # Send data
+        print("Querying GPS for message...")
+        message = gps.readResponse()
+        print("sending msg: " + str(message))
+        sock.sendall(message[1])
+        '''
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
 
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            if (not (data == "")):
+                print("receiving data:")
+                print(data)
+                id = data[0]
+                payload = data[1:-1]
+                gps.sendCmd(id,payload)
+        '''
+    finally:
+        print("closing socket")
+        #sock.close()
