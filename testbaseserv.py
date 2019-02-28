a87d74a06c3d033e55820a0ba100a54332088d26
Aubrey Anderson <aubrey@bigsur.com>
2019-02-28 11:19:20 -0500

    Fix some pylint errors

--- testbaseserv.py
+++ testbaseserv.py
@@ -1,17 +1,21 @@
-import socket, select, sys, _thread, subprocess, time
+#!/bin/python3
+import socket
+import select
+import sys
+import subprocess
+import time
+import _thread
 
 # Getting a random free tcp port in python using sockets
 def get_free_tcp_port():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp.bind(('', 0))
-    addr, port = tcp.getsockname()
+    port = tcp.getsockname()[1]
     tcp.close()
     return port
 
 #path to config file (without I/O params)
 CONFIG_FILE = '../rtk_test.conf'
-#config file data
-config = ""
 
 #static IP address for base station
 IP = '192.168.4.1'
@@ -39,12 +43,12 @@ aggserv_conn_list = []
 def eprint(mess, func_name):
     sys.stdout.write('[' + func_name + ']: ' + mess + '\n')
 
-def handleTrackerConnections():
+def handle_tracker_connections():
     #TODO try/catch/finally for disconnections
     while True:
         for connection in trackserv_conn_list:
             data = connection.recv(4096)
-            if (data != ""):
+            if data != "":
                 #TODO for testing
                 tracker_id = "T1234"
                 #print(len(data))
@@ -64,8 +68,8 @@ def handleTrackerConnections():
                 trackserv_conn_list.remove(connection)
                 tracker_input_map[tracker_id].close()
                 del tracker_input_map[tracker_id]
-    
-def runTrackerServer():
+
+def run_tracker_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #60 second timeout
@@ -89,7 +93,7 @@ def runTrackerServer():
             output_port = get_free_tcp_port()
             #spawn RTKLIB process with these params
             _thread.start_new_thread(output_sock, (tracker_id, output_port))
-            spawnRTKLIB(input_port, output_port, tracker_id)
+            spawn_rtklib(input_port, output_port, tracker_id)
             time.sleep(3)
             input_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             input_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -100,8 +104,8 @@ def runTrackerServer():
             tracker_input_map[tracker_id] = input_sock
             trackserv_conn_list.append(connection)
 
-def spawnRTKLIB(input_port, output_port, tracker_id):
-    new_config = config
+def spawn_rtklib(input_port, output_port, tracker_id):
+    new_config = CONFIG
     #set input
     new_config += "\ninpstr1-path       =127.0.0.1:" + str(input_port)
     #set output
@@ -115,7 +119,7 @@ def spawnRTKLIB(input_port, output_port, tracker_id):
     args = ['./rtkrcv', '-s', '-o', new_file_name]
     subprocess.Popen(args, stdout=subprocess.DEVNULL)
 
-def runAggregatorServer():
+def run_aggregator_server():
     aggregator = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     aggregator.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     eprint('(' + AGGSERV_ADDR[0] + ":" + str(AGGSERV_ADDR[1]) + '): starting up aggregator server', AGGSERV)
@@ -137,8 +141,8 @@ def output_sock(tracker_id, output_port):
     sock.bind(server_address)
     #allow only one connection to this output server
     sock.listen(1)
-    eprint (tracker_id + '(' + server_address[0] + ":" + str(server_address[1]) + '): started up RTKLIB output server', RTKOUTSERV)
-    connection, client_address = sock.accept()
+    eprint(tracker_id + '(' + server_address[0] + ":" + str(server_address[1]) + '): started up RTKLIB output server', RTKOUTSERV)
+    connection = sock.accept()[0]
     eprint(tracker_id + ': output connection established with RTKLIB', RTKOUTSERV)
     while True:
         print("CHECKING")
@@ -149,44 +153,41 @@ def output_sock(tracker_id, output_port):
                 print("READING")
                 data = connection.recv(1024)
                 print("DONE READING")
-            except sock.error as e:
-                eprint(tracker_id + ': socket error: ' + str(e), RTKOUTSERV)
+            except socket.error as err:
+                eprint(tracker_id + ': socket error: ' + str(err), RTKOUTSERV)
                 break
             except:
-                print ("Unexpected error: ", sys.exc_info()[0])
+                print("Unexpected error: ", sys.exc_info()[0])
                 raise
-            if (data != b''):
+            if data != b'':
                 eprint(tracker_id + ': RTKLIB message: "' + str(data) + '"', RTKOUTSERV)
                 #ignore message if first character is % (RTKLIB headers)
-                if (data[0] != 37):
+                if data[0] != 37:
                     print("SENDING DATA")
-                    parsed_data = parseDataFromRTKLIB(tracker_id, data)
+                    parsed_data = parse_data_from_rtklib(tracker_id, data)
                     for ext_conn in aggserv_conn_list:
                         print("SENDING DATA LOOP")
-                        ext_conn.sendall(data)
+                        ext_conn.sendall(parsed_data)
             else:
                 eprint(tracker_id + ': socket closed', RTKOUTSERV)
                 break
     connection.close()
 
-def parseDataFromRTKLIB(tracker_id, data):
+def parse_data_from_rtklib(tracker_id, data):
     #input format example: '2038, 417928.999, 1.000, 2.000, 3.000, 5, 7, 6.6558, 3.1100, 2.8179, -3.3301, 1.9243, -3.2028, 0.00, 0.0'
     #output format example: 'T1234,0001,0000001.000,000000001.0000,000000002.0000'
     data_parts = data.decode('UTF-8').split(',')
-    GPST_week = "{:4.0f}".format(float(data_parts[0]))
-    GPST_seconds = "{:10.3f}".format(float(data_parts[1]))
-    e = "{:14.4f}".format(float(data_parts[2]))
-    n = "{:14.4f}".format(float(data_parts[3]))
-    return (str(tracker_id) + "," + str(GPST_week) + "," + str(GPST_seconds) + "," + str(e) + "," + str(n)).encode('UTF-8')
+    gpst_week = "{:4.0f}".format(float(data_parts[0]))
+    gpst_seconds = "{:10.3f}".format(float(data_parts[1]))
+    east = "{:14.4f}".format(float(data_parts[2]))
+    north = "{:14.4f}".format(float(data_parts[3]))
+    out_str = "{},{},{},{},{}".format(tracker_id, gpst_week, gpst_seconds, east, north)
+    return out_str.encode('UTF-8')
 
 
-with open(CONFIG_FILE, 'r') as myfile:
-    config = myfile.read()
-_thread.start_new_thread(runAggregatorServer, ())
-_thread.start_new_thread(handleTrackerConnections, ())
-runTrackerServer()
-
-
-
-    
-
+if __name__ == "__main__":
+    with open(CONFIG_FILE, 'r') as myfile:
+        CONFIG = myfile.read()
+    _thread.start_new_thread(run_aggregator_server, ())
+    _thread.start_new_thread(handle_tracker_connections, ())
+    run_tracker_server()
